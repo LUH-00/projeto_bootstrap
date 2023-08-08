e9b047a51eeab2d46e5a4b67aaf2185b06d1097a
Luan Vinicius <luanvmc.000@gmail.com>
2023-08-07 21:11:58 -0300

    iniciado validação de email

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ let nome = document.querySelector("#nome")
 
 function exibirNome(){
     console.log(nome.value)
-    if(nome.value = ""){
+    if(nome.value == ""){
         nome.style.border = "2px solid red"
     }
     else{
@@ -19,3 +19,42 @@ nome.addEventListener("blur", exibirNome) //o addEventListener cria um evento pa
 // nome.addEventListener("focus", function(){
 //     nome.value = "Este campo não deve ficar vazio"
 // })
+
+let idade = document.querySelector("#idade")
+idade.addEventListener("blur" ,function(){
+        if(idade.value >=18){
+            alert("Você possui 18 anos ou mais!")
+    }
+    else{
+        alert("Infelizmente você não é de maior!")
+        // Essa função irá redirecionar o usuário para um link específico
+        window.location.href = "https://www.youtube.com/watch?v=5P8GcCpmGYQ"
+    }
+    
+})
+
+let endereco = document.querySelector("input[name=end]")
+endereco.addEventListener("blur", function(){
+    console.log(endereco.value.length)
+    if(endereco.value.length  >= 35){
+        alert ("O endereço está muito grande")
+        endereco.style.border = "2px solid red"
+    }
+    else {
+        alert ("O endereço está OK!")
+        endereco.style.border = "2px solid yellow"
+    }
+})
+
+
+let email = document.querySelector("#email")
+let confirmar = document.querySelector("#confirmar")
+let minusculo = email.value.toLowerCase()// tornando o conteúdo do E-mail minúsculo
+
+email.addEventListener("keyup", function(){
+    confirmar.value = email.value
+    if(minusculo.indexOf("@") == -1 || minusculo.indexOf(".com") == -1){}
+    else{}
+    console.log (email.value.indexOf("@"))
+})
+
